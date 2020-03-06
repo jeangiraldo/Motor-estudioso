232784997ca2bbdc29e1d6f200b72cba29667f70
jeangiraldo <61886113+jeangiraldo@users.noreply.github.com>
2020-03-06 14:22:32 -0500

    Add files via upload

---
+++ erro python.py
@@ -0,0 +1,5 @@
+import math
+x=float(input("Enter x:"))
+y=math.sqrt(x)
+print("The square root of", x,)
+
---
+++ excepciones python.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 29 12:49:17 2020
+
+@author: CEC
+"""
+
+try:
+    print("1")
+    x=1/0
+    print("2")
+except:
+    print("Oh dear, something went wrong...")
+    
+print("3")
